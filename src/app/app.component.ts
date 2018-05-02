8ade7b34abe89906d838d58b14e9cc4f6d426be1
Isaac Shoman and Ted Leary <ted@>
2018-05-02 09:28:10 -0700

    Add input and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './models/task.model';
+import { TaskListComponent } from './task-list/task-list.component'
 
 @Component({
   selector: 'crazy-app-selector',
@@ -12,23 +13,21 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  tasks: Task[] = [
+  // selectedTask = null;
+
+
+  masterTaskList: Task[] = [
     new Task('Finish weekend Angular homeworkd for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = this.masterTaskList;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
+
+  finishedEditing() {
+  this.selectedTask = null;
+}
 }
